c97b154fee13468df4bc6fe615587efc0a1767cd
Nikita Biryukov <nbiryukov96@gmail.com>
2018-09-25 16:45:32 +0300

    Приложение зарегестрированно в ВК и сделано api для ввода логина и пароля.

--- src/main/java/org/vkauth/controller/MainController.java
+++ src/main/java/org/vkauth/controller/MainController.java
@@ -2,6 +2,9 @@ package org.vkauth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
@@ -11,5 +14,16 @@ public class MainController {
     @GetMapping
     public String main() {
         return "index";
+    }
+
+    @PostMapping(value = "/auth")
+    public ModelAndView openAuth() {
+        
+        return new ModelAndView(new RedirectView("https://oauth.vk.com/authorize" +
+                "?client_id=6702883&display=popup" +
+                "&redirect_uri=https://authvk.herokuapp.com/lk" +
+                "&scope=friends" +
+                "&response_type=code" +
+                "&v=5.85"));
     }
 }
